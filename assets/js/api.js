b5e6f9e68d66a655631320dde006c622a4fa1141
Luan Almeida <106840952+luanalmeida18@users.noreply.github.com>
2023-03-27 21:10:36 -0300

    Update Profile JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/luanalmeida18/js-developer-portfolio/main/data/profile.json';
+  const url = 'https://raw.githubusercontent.com/luanalmeida18/portfolioLuanAlmeida/master/data/profile.json';
   const response = await fetch(url)
   const profileData = await response.json()
   return profileData
